67d6112bcac7070a374bea4fbe2a19dffbac5270
dafahfabiansyah <davarobiansyah@gmail.com>
2023-09-07 12:01:21 +0700

    update

--- app.js
+++ app.js
@@ -18,6 +18,6 @@ app.use((req, res, next) => {
   });
 });
 
-// app.listen(3000, () => {
-//   console.log('Server is running on port 3000');
-// });
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
